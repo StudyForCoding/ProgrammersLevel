c941bceb4b9e4ae8ac22879e371a9a1efba45271
hakamma <hakamma2@naver.com>
2021-07-19 10:16:58 +0900

    Create hakamma.py

---
+++ Level1/Lessons77884/hakamma.py
@@ -0,0 +1,17 @@
+#77884 ¾à¼öÀÇ °³¼ö¿Í µ¡¼À
+
+def solution(left, right):
+    answer = 0
+    
+    for i in range(left, right+1):
+        num = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                num += 1
+                
+        if num % 2 == 0:
+            answer += i
+        else:
+            answer -= i
+    
+    return answer
