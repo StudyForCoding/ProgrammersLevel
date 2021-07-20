7e2833b923c5cf92e5d65969dfef08398dac5703
najy97 <57242033+najy97@users.noreply.github.com>
2021-07-20 10:00:49 +0900

    0720

--- Level2/Lessons12913/12913_njy.py
+++ Level2/Lessons12913/12913_njy.py
@@ -1,8 +1,37 @@
 #땅따먹기
 
-land = [[1,2,3,5],[5,6,7,8],[4,3,2,1]]
+def solution(land):
+    size = len(land)-1
+    for i in range(0, size):
+        land[i+1][0] += max(land[i][1],land[i][2],land[i][3])
+        land[i+1][1] += max(land[i][0],land[i][2],land[i][3])
+        land[i+1][2] += max(land[i][0],land[i][1],land[i][3])
+        land[i+1][3] += max(land[i][0],land[i][1],land[i][2])
+    return max(land[size])
 
-def choose_land(idx,land):
-    for i in range(len(land[0])):
-        if i!=idx:
-            
+"""
+정확성  테스트
+테스트 1 〉	통과 (1.57ms, 10.4MB)
+테스트 2 〉	통과 (1.74ms, 10.3MB)
+테스트 3 〉	통과 (1.63ms, 10.3MB)
+테스트 4 〉	통과 (1.81ms, 10.3MB)
+테스트 5 〉	통과 (1.67ms, 10.4MB)
+테스트 6 〉	통과 (1.60ms, 10.3MB)
+테스트 7 〉	통과 (2.43ms, 10.4MB)
+테스트 8 〉	통과 (1.88ms, 10.4MB)
+테스트 9 〉	통과 (1.56ms, 10.2MB)
+테스트 10 〉	통과 (1.71ms, 10.4MB)
+테스트 11 〉	통과 (1.82ms, 10.4MB)
+테스트 12 〉	통과 (1.81ms, 10.3MB)
+테스트 13 〉	통과 (1.60ms, 10.3MB)
+테스트 14 〉	통과 (1.75ms, 10.3MB)
+테스트 15 〉	통과 (1.91ms, 10.3MB)
+테스트 16 〉	통과 (1.78ms, 10.3MB)
+테스트 17 〉	통과 (1.85ms, 10.4MB)
+테스트 18 〉	통과 (1.50ms, 10.4MB)
+효율성  테스트
+테스트 1 〉	통과 (138.44ms, 32.5MB)
+테스트 2 〉	통과 (123.75ms, 32.4MB)
+테스트 3 〉	통과 (122.34ms, 32.4MB)
+테스트 4 〉	통과 (135.32ms, 32.4MB)
+"""
--- Level2/Lessons17686/17686_njy.py
+++ Level2/Lessons17686/17686_njy.py
@@ -1,1 +1,48 @@
 #파일명 정렬
+def solution(files):
+    answer=[]
+    str=[]
+
+    for file in files:
+        HEAD=''
+
+        for char in file:
+            if char.isdigit():
+                break
+            HEAD+=char
+
+        NUM=''        
+        for char in file[len(HEAD):]:
+            if not char.isdigit():
+                break
+            NUM+=char
+
+        str.append([HEAD.lower(),int(NUM),file]) # 대소문자 구분 안하기 + 숫자로 비교 + 원래 파일명
+
+    file_list = sorted(str, key=lambda x:(x[0],x[1])) # HEAD와 NUM 기준으로 정렬
+
+    for i in file_list:
+        answer.append(i[2]) # 파일명만 반환
+    return answer
+    """
+    테스트 1 〉	통과 (0.04ms, 10.4MB)
+    테스트 2 〉	통과 (0.04ms, 10.4MB)
+    테스트 3 〉	통과 (2.34ms, 10.7MB)
+    테스트 4 〉	통과 (2.26ms, 10.6MB)
+    테스트 5 〉	통과 (2.14ms, 10.7MB)
+    테스트 6 〉	통과 (2.48ms, 10.7MB)
+    테스트 7 〉	통과 (2.15ms, 10.7MB)
+    테스트 8 〉	통과 (2.31ms, 10.6MB)
+    테스트 9 〉	통과 (2.03ms, 10.7MB)
+    테스트 10 〉	통과 (4.43ms, 10.8MB)
+    테스트 11 〉	통과 (2.04ms, 10.5MB)
+    테스트 12 〉	통과 (2.20ms, 10.8MB)
+    테스트 13 〉	통과 (1.83ms, 10.8MB)
+    테스트 14 〉	통과 (2.15ms, 10.8MB)
+    테스트 15 〉	통과 (2.05ms, 10.8MB)
+    테스트 16 〉	통과 (4.34ms, 10.8MB)
+    테스트 17 〉	통과 (1.81ms, 10.8MB)
+    테스트 18 〉	통과 (1.73ms, 10.7MB)
+    테스트 19 〉	통과 (2.26ms, 10.8MB)
+    테스트 20 〉	통과 (1.96ms, 10.8MB)
+    """
