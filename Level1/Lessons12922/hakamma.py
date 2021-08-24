10ec5273743d7ca3a55dc73e0d5c88ba5896a837
hakamma <hakamma2@naver.com>
2021-08-24 14:29:21 +0900

    Create hakamma.py

---
+++ Level1/Lessons12922/hakamma.py
@@ -0,0 +1,10 @@
+#12922 수박수박수박수?
+
+def solution(n):
+    answer = ''
+    for i in range(n):
+        if i%2 == 0:
+            answer += "수"
+        elif i%2 == 1:
+            answer += "박"
+    return answer
